756f49ac11a18da885a88ed07eb91bf6142c5ad4
JulianRiedinger7 <julianriedinger27@gmail.com>
2023-06-16 19:38:26 -0300

    BaSP-W14: created performance glitch & standard user tests

--- test/specs/logine2e.js
+++ test/specs/logine2e.js
@@ -34,4 +34,30 @@ describe('Login functionality', () => {
     await expect(LoginPage.errorMessage).toBeDisplayed()
     await expect(LoginPage.errorMessage).toHaveTextContaining('Epic sadface: Sorry, this user has been locked out.')
   })
+
+  it('should login within a specified duration', async () => {
+    const startTime = new Date().getTime()
+
+    await LoginPage.login('performance_glitch_user', validPassword)
+
+    const endTime = new Date().getTime()
+    const duration = endTime - startTime
+
+    const maxDuration = 4000
+
+    expect(duration).toBeLessThan(maxDuration, `Login took longer than ${maxDuration} miliseconds`)
+    await ProductsPage.logout()
+  })
+
+  it('should login correctly and logout', async () => {
+    await LoginPage.login('standard_user', validPassword)
+    await expect(ProductsPage.productsTitle).toBeDisplayed()
+    await expect(ProductsPage.hamburgerMenu).toBeDisplayed()
+
+    await expect(ProductsPage.productsTitle).toHaveTextContaining('Products')
+    await ProductsPage.logout()
+    await expect(LoginPage.usernameInput).toBeDisplayed()
+    // luego chequear de hacer primero todos los casos negativos, y por ultimo este caso positivo.
+    // y que este caso positivo no se desloguee, sino que en otro describe aprovechemos que esta logueado para poder hacer una compra
+  })
 })
